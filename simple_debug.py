759d3ffae91276d892de74962a895688212bb514
Tong Li <tongli@Tongs-MacBook-Pro.local>
2025-06-25 13:48:08 -0400

    re-edit debug (for speed test)

--- simple_debug.py
+++ simple_debug.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-
+import time
 import bayes_model as bm
 import policy as pol
 import sim_wrapper as sw
-
+from joblib import Parallel, delayed
 
 #################### test
 #############################
@@ -16,9 +16,9 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 hyperparams = sw.HyperParams(
-    n_rep=10000,
+    n_rep=400,
     n_arm=3,
-    horizon=1000,
+    horizon=10000,
     burn_in=5,
     batch_size = None,
     fast_batch_epsilon = 0.1,
@@ -33,11 +33,13 @@ arr_dim = sw.ArrDim({'n_rep':0,'horizon':1,'n_arm':-1},hyperparams)
 # simple simulation
 bandit = pol.StoBandit(reward_model=sw.RewardModel(model=np.random.binomial,
                                                    parameters={'n': [1,1,1], 'p': [0.6,0.5,0.5]})) #simulate the case where there's 3 arms, with Bernoulli reward mean = 0.5
-import time
+params_dict = {'policy':bandit.ts_postdiff_ur,'algo_para':0.3, 'hyperparams':hyperparams}
+
+n_loop = 10
 
 start_time = time.time()
 
-for i in range(5):
+for i in range(n_loop):
     res1 = sw.run_simulation(
         policy=bandit.ts_postdiff_ur,
         algo_para=0.3,
@@ -46,9 +48,14 @@ for i in range(5):
 
 end_time = time.time()
 
-print(f"Total time for 10 runs: {end_time - start_time:.2f} seconds")
+print(f"Total time for regular: {end_time - start_time:.2f} seconds")
 
+start_time = time.time()
+Parallel(n_jobs=-1)(delayed(sw.run_simulation)(**params_dict) for _ in range(n_loop))
 
+end_time = time.time()
+
+print(f"Total time for paraller: {end_time - start_time:.2f} seconds")
 #simulation result is saved in res
 #check its method such as:
 #res.reward_hist ; res.arm_counts ; etc
