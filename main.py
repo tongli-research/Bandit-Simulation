8ba9f7dce17ef2b7fcb90252e3af91fef27fffed
Tong Li <tongli@tongs-mbp.home>
2025-07-20 16:33:04 -0400

    organized for vm sim

--- main.py
+++ main.py
@@ -30,10 +30,6 @@ import bayes_vector_ops as bayes
 logging.getLogger("ax.service.managed_loop").setLevel(logging.CRITICAL)
 logging.getLogger('ax.generation_strategy.dispatch_utils').setLevel(logging.CRITICAL)
 
-# TODO: change min_effect
-import faulthandler
-
-faulthandler.enable()
 
 # for vm?
 import sys
@@ -49,23 +45,23 @@ import os
 
 # np.random.seed(0)
 sim_config_base = SimulationConfig(
-    n_rep=100,
+    n_rep=50000,
     n_arm=3,
-    horizon=1500,  # max horizon to try in simulation
+    horizon=3000,  # max horizon to try in simulation
     burn_in_per_arm=5,
     # horizon_check_points=sw.generate_quadratic_schedule(2000), #can ignore for now... TODO: see where I used it (and delete if not)
     # can set tuning_density to make the schedule denser / looser
-    n_opt_trials=5,  # TODO: optimize for this in our code
+    n_opt_trials=15,  # TODO: optimize for this in our code
     # arm_mean_reward_dist_loc = [0.7,0.3,0.3],
     # arm_mean_reward_dist_scale = 0.01,
     test_procedure=ANOVA(),
-    step_cost=1,
+    step_cost=0.05,
     reward_evaluation_method='regret',
-    vector_ops=bayes.BackendOpsNP()
+    vector_ops=bayes.BackendOpsTF()
 )
 # algo_list = ['ts_adapt_explor','ts_postdiff_top','eps_ts', 'ts_postdiff_ur','ts_probclip']
 algo_list = [algo.TSProbClip,algo.TSPostDiffTop, algo.TSPostDiffUR, algo.EpsTS,algo.Top2TS] #TODO: import directly
-algo_param_list = list(np.arange(0, 1.001, 0.05))
+algo_param_list = list(np.arange(0, 1.001, 0.02))
 
 test_list = [
     ANOVA(),
@@ -77,10 +73,10 @@ test_list = [
     Tukey(test_type='distinct-best-arm'),
 ]
 
-step_cost_list = [0.01,0.02]  # ,0.02,0.05,0.1,0.2]
+step_cost_list = [0.001,0.002,0.005,0.01,0.02,0.05,0.1,0.2,0.5,1]
 
-loc_list = [0.2,0.3,0.4,0.5,0.6,0.7,0.8]
-scale_list = [0.1,0.125, 0.15,0.175, 0.2]
+loc_list = [0.2,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.8]
+scale_list = [0.08,0.10,0.12,0.135, 0.15,0.165, 0.18,0.20,0.22]
 
 
 
@@ -99,9 +95,12 @@ def run_main_task(i, j, k):
     sim_config.manual_init()
     params, val, _, _, sim_result_keeper = sw.optimize_algorithm(sim_config, algo=algo,algo_param_list=algo_param_list)
     # return (i, j, algo, params, val, sim_result_keeper.get((algo, params['algo_para']), None))
-    return {'setting': sim_config.setting_signature,
+    return {'test':sim_config.test_procedure.test_signature,
+            'step_cost': sim_config.step_cost,
+            'mean_reward_dist_loc':sim_config.arm_mean_reward_dist_loc[0],
+            'mean_reward_dist_scale':sim_config.arm_mean_reward_dist_scale[0],
             'algo_name': algo.__name__,
-            'algo_params': params['algo_para'],
+            'algo_param': params['algo_para'],
             **sim_result_keeper.get((algo.__name__, params['algo_para'], sim_config.setting_signature), None),
             'all_results': sim_result_keeper,
             }
@@ -113,10 +112,12 @@ def run_loc_mismatch_task(i, j):
     sim_config.manual_init()
     params, val, _, _, sim_result_keeper = sw.optimize_algorithm(sim_config, algo=algo,algo_param_list=None)
     # return (i, j, algo, params, val, sim_result_keeper.get((algo, params['algo_para']), None))
-    return {'setting': sim_config.setting_signature,
-            'loc':loc_list[i],
+    return {'test': sim_config.test_procedure.test_signature,
+            'step_cost': sim_config.step_cost,
+            'mean_reward_dist_loc': sim_config.arm_mean_reward_dist_loc[0],
+            'mean_reward_dist_scale': sim_config.arm_mean_reward_dist_scale[0],
             'algo_name': algo.__name__,
-            'algo_params': params['algo_para'],
+            'algo_param': params['algo_para'],
             **sim_result_keeper.get((algo.__name__, params['algo_para'], sim_config.setting_signature), None),
             'all_results': sim_result_keeper,
             }
@@ -129,10 +130,12 @@ def run_scale_mismatch_task(i, j):
     sim_config.manual_init()
     params, val, _, _, sim_result_keeper = sw.optimize_algorithm(sim_config, algo=algo,algo_param_list=None)
     # return (i, j, algo, params, val, sim_result_keeper.get((algo, params['algo_para']), None))
-    return {'setting': sim_config.setting_signature,
-            'scale':scale_list[i],
+    return {'test': sim_config.test_procedure.test_signature,
+            'step_cost': sim_config.step_cost,
+            'mean_reward_dist_loc': sim_config.arm_mean_reward_dist_loc[0],
+            'mean_reward_dist_scale': sim_config.arm_mean_reward_dist_scale[0],
             'algo_name': algo.__name__,
-            'algo_params': params['algo_para'],
+            'algo_param': params['algo_para'],
             **sim_result_keeper.get((algo.__name__, params['algo_para'], sim_config.setting_signature), None),
             'all_results': sim_result_keeper,
             }
@@ -141,13 +144,16 @@ def rerun_loc_mismatch_task(j):
     sim_config = copy.deepcopy(sim_config_base)
     algo = algo_list[j]
     sim_config.manual_init()
-    algo_param_list = list(loc_best_df.algo_params[loc_best_df.algo_name == algo.__name__])
+    algo_param_list = list(loc_best_df.algo_param[loc_best_df.algo_name == algo.__name__])
     params, val, _, _, sim_result_keeper = sw.optimize_algorithm(sim_config, algo=algo,algo_param_list=algo_param_list)
 
     # return (i, j, algo, params, val, sim_result_keeper.get((algo, params['algo_para']), None))
-    return {'setting': sim_config.setting_signature,
+    return {'test': sim_config.test_procedure.test_signature,
+            'step_cost': sim_config.step_cost,
+            'mean_reward_dist_loc': sim_config.arm_mean_reward_dist_loc[0],
+            'mean_reward_dist_scale': sim_config.arm_mean_reward_dist_scale[0],
             'algo_name': algo.__name__,
-            'algo_params': params['algo_para'],
+            'algo_param': params['algo_para'],
             **sim_result_keeper.get((algo.__name__, params['algo_para'], sim_config.setting_signature), None),
             'all_results': sim_result_keeper,
             }
@@ -157,12 +163,15 @@ def rerun_scale_mismatch_task(j):
     algo = algo_list[j]
     sim_config.manual_init()
 
-    algo_param_list = list(scale_best_df.algo_params[scale_best_df.algo_name == algo.__name__])
+    algo_param_list = list(scale_best_df.algo_param[scale_best_df.algo_name == algo.__name__])
     params, val, _, _, sim_result_keeper = sw.optimize_algorithm(sim_config, algo=algo, algo_param_list=algo_param_list)
     # return (i, j, algo, params, val, sim_result_keeper.get((algo, params['algo_para']), None))
-    return {'setting': sim_config.setting_signature,
+    return {'test': sim_config.test_procedure.test_signature,
+            'step_cost': sim_config.step_cost,
+            'mean_reward_dist_loc': sim_config.arm_mean_reward_dist_loc[0],
+            'mean_reward_dist_scale': sim_config.arm_mean_reward_dist_scale[0],
             'algo_name': algo.__name__,
-            'algo_params': params['algo_para'],
+            'algo_param': params['algo_para'],
             **sim_result_keeper.get((algo.__name__, params['algo_para'], sim_config.setting_signature), None),
             'all_results': sim_result_keeper,
             }
@@ -192,6 +201,7 @@ def extract_results(results):
 
         for (algo, param, _), result in all_result_dict.items():
             entry = row_info.copy()
+            entry['best_algo_param'] = entry['algo_param']
             entry['algo_name'] = algo
             entry['algo_param'] = param
             entry.update({k: float(v) for k, v in result.items()})
@@ -205,7 +215,6 @@ main_results = Parallel(n_jobs=-1)(delayed(run_main_task)(i, j, k) for i, j, k i
 loc_results = Parallel(n_jobs=-1)(delayed(run_loc_mismatch_task)(i, j) for i, j in loc_task_list)
 scale_results = Parallel(n_jobs=-1)(delayed(run_scale_mismatch_task)(i, j) for i, j in scale_task_list)
 
-
 main_best_df, main_full_df = extract_results(main_results)
 loc_best_df, loc_full_df = extract_results(loc_results)
 scale_best_df, scale_full_df = extract_results(scale_results)
@@ -216,7 +225,26 @@ rerun_loc_mismatch_best_df, rerun_loc_mismatch_full_df = extract_results(rerun_l
 rerun_scale_mismatch_best_df, rerun_scale_mismatch_full_df = extract_results(rerun_scale_mismatch_results)
 
 
-# best_df.to_csv('best_df0719.csv')
-# full_df.to_csv('full_df0719.csv')
-# best_df.to_csv('~/best_df0718.csv')
-# full_df.to_csv('~/full_df0718.csv')
+# === Save all DataFrames to CSV ===
+output_dir = "output_results"
+os.makedirs(output_dir, exist_ok=True)
+
+dfs = {
+    "main_best_df": main_best_df,
+    "main_full_df": main_full_df,
+    "loc_best_df": loc_best_df,
+    "loc_full_df": loc_full_df,
+    "scale_best_df": scale_best_df,
+    "scale_full_df": scale_full_df,
+    "rerun_loc_mismatch_best_df": rerun_loc_mismatch_best_df,
+    "rerun_loc_mismatch_full_df": rerun_loc_mismatch_full_df,
+    "rerun_scale_mismatch_best_df": rerun_scale_mismatch_best_df,
+    "rerun_scale_mismatch_full_df": rerun_scale_mismatch_full_df,
+}
+
+for name, df in dfs.items():
+    df.to_csv(os.path.join(output_dir, f"{name}.csv"), index=False)
+
+# === Save all variables to a pickle file ===
+with open(os.path.join(output_dir, "all_results.pkl"), "wb") as f:
+    pickle.dump(dfs, f)
